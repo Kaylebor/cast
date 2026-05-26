09fa44bf57d1a52a8092e23e97ccd156b7996487
Ender Veiga Bueno <letstalk@ender.codes>
2026-05-26 14:01:29 +0200

    fix: string match must use -q in keybind context to suppress output to commandline

--- functions/__cast_keybind_complete.fish
+++ functions/__cast_keybind_complete.fish
@@ -4,7 +4,7 @@ function __cast_keybind_complete
     set -q cast_debug; and set debug --debug
 
     # Detect codify signal: line starts with # (natural language task)
-    if string match -r '^#\s*' -- $buf
+    if string match -q -r '^#\s*' -- $buf
         set -l result (cast_codify $buf $debug 2>/dev/null)
         if test $status -eq 0 -a -n "$result"
             commandline -r -- $result
